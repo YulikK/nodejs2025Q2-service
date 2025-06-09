a565b88400f0e09ffe2400d5c661a54edd6046f3
YulikK <yulikpoch@gmail.com>
2025-06-09 11:16:32 +0200

    feat: enhance entities with Expose decorators and update track DTOs for improved serialization

--- src/track/entities/track.entity.ts
+++ src/track/entities/track.entity.ts
@@ -1,27 +1,35 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { Artist } from '../../artist/entities/artist.entity';
 import { Album } from '../../album/entities/album.entity';
+import { Expose } from 'class-transformer';
 
 @Entity('tracks')
 export class Track {
   @PrimaryGeneratedColumn('uuid')
+  @Expose()
   id: string;
 
   @Column()
+  @Expose()
   name: string;
 
   @ManyToOne(() => Artist, { nullable: true, onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'artist_id' })
   artist: Artist;
 
-  @Column({ nullable: true })
+  @Column({ name: 'artist_id', nullable: true })
+  @Expose()
   artistId: string;
 
   @ManyToOne(() => Album, { nullable: true, onDelete: 'SET NULL' })
+  @JoinColumn({ name: 'album_id' })
   album: Album;
 
-  @Column({ nullable: true })
+  @Column({ name: 'album_id', nullable: true })
+  @Expose()
   albumId: string;
 
   @Column()
+  @Expose()
   duration: number;
 }
